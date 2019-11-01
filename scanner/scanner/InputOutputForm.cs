eeeefa3313e0da286c3d7a6c496161dbd7e2e644
EL-SHREIF <36492402+EL-SHREIF@users.noreply.github.com>
2019-11-01 22:56:15 +0200

    add new options

--- scanner/scanner/InputOutputForm.cs
+++ scanner/scanner/InputOutputForm.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace scanner
 {
     public partial class InputOutputForm : Form
     {
+        int out_no;
+        scanner1 sc;
+        StreamWriter writer;
         public InputOutputForm()
         {
             InitializeComponent();
+            out_no = 1;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string f = textBox1.Text;
+            try
+            {
+                string output = "output" + out_no.ToString() + ".txt";
+                writer = new StreamWriter(output);
+                sc = new scanner1(f, writer);
+                int done = sc.scan();
+                writer.Close();
+                if (done == 0)
+                {
+                    List<string> tokns = new List<string>();
+                    List<string> values = new List<string>();
+                    int counter_of_tokens = 0;
+                    string file_text = File.ReadAllText(output);
+                    for (int i = 0; i < file_text.Length; i++)
+                    {
+                        if (i == file_text.Length - 1)
+                            break;
+                        char cur = file_text[i];
+                        string name1 = "";
+                        while (cur != ',') {
+                            if(cur != ' ' && cur != '\n' && cur != '\t' && cur != '\r')
+                                name1 += cur;
+                            i++;
+                            cur = file_text[i];
+                        }
+                        i++;
+                        cur = file_text[i];
+                        i++;
+                        cur = file_text[i];
+                        string name2 = "";
+                        while (cur != ' ' && cur != '\n' && cur != '\t' && cur != '\r')
+                        {
+                            if (cur != ' ' && cur != '\n' && cur != '\t' && cur != '\r')
+                                name2 += cur;
+                            i++;
+                            cur = file_text[i];
+                        }
+                        tokns.Add(name1);
+                        values.Add(name2);
+                        counter_of_tokens++;
+
+                    }
+                    Output form = new Output(tokns,values,counter_of_tokens);
+                    form.Show();
+                    Hide();
+                }
+                else
+                {
+                    label2.Text = "syntax error";
+                    label2.ForeColor = Color.Red;
+                }
+            }
+            catch
+            {
+                writer.Close();
+            }
+        }
+
+        private void Btn_back_Click(object sender, EventArgs e)
+        {
+            SelectMode form = new SelectMode();
+            form.Show();
+            Hide();
         }
     }
 }
---
+++ scanner/scanner/Output.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scanner
+{
+    public partial class Output : Form
+    {
+        public Output(List<string> t, List<string> v,int k)
+        {
+            InitializeComponent();
+            Label old_lb = new Label();
+            old_lb = this.lbl_p1;
+            int y = 60;
+            for (int i = 0; i < k; i++)
+            {
+                Label new_lb = new Label();
+                new_lb.Size = old_lb.Size;
+                new_lb.Visible = true;
+                new_lb.Location = new Point(old_lb.Location.X, old_lb.Location.Y + y - 60);
+                new_lb.Text = t[i];
+                new_lb.Font = old_lb.Font;
+                new_lb.BackColor = old_lb.BackColor;
+                new_lb.TextAlign = old_lb.TextAlign;
+                new_lb.Visible = true;
+                this.groupBox_processDetail.Controls.Add(new_lb);
+                y = y + 60;
+            }
+
+
+            old_lb = new Label();
+            old_lb = this.label2;
+            y = 60;
+            for (int i = 0; i < k; i++)
+            {
+                Label new_lb = new Label();
+                new_lb.Size = old_lb.Size;
+                new_lb.Location = new Point(old_lb.Location.X, old_lb.Location.Y + y - 60);
+                new_lb.Visible = true;
+                new_lb.Text = v[i];
+                new_lb.Font = old_lb.Font;
+                new_lb.BackColor = old_lb.BackColor;
+                new_lb.TextAlign = old_lb.TextAlign;
+                new_lb.Visible = true;
+                this.groupBox_processDetail.Controls.Add(new_lb);
+                y = y + 60;
+            }
+        }
+
+        private void Btn_back_Click(object sender, EventArgs e)
+        {
+            SelectMode form = new SelectMode();
+            form.Show();
+            Hide();
+        }
+
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
